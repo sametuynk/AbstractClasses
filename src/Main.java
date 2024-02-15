ccd79f4fe841faf4579196d8e6708f19eea6d883
samet.uyanik <samet.uyanik2756@gmail.com>
2024-02-15 14:37:29 +0300

    abstract work done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        * Abstract sınıflar asla 'NEW' edilemezler.
+        *
+        * */
+        WomenGameCalculator womenGameCalculator=new WomenGameCalculator();
+        womenGameCalculator.hesapla();
+        womenGameCalculator.gameOver();
+
+        GameCalculator gameCalculator=new KidsGameCalculator();
+        gameCalculator.hesapla();
+
+
+
+
+
+
+
+
+
+
+
+//        Kullanışsız
+//        GameCalculator gameCalculator=new GameCalculator() {
+//            @Override
+//            public void hesapla() {
+//
+//            }
+//        };
     }
 }
---
+++ src/WomenGameCalculator.java
@@ -0,0 +1,6 @@
+public class WomenGameCalculator extends GameCalculator{
+    @Override
+    public void hesapla() {
+
+    }
+}
